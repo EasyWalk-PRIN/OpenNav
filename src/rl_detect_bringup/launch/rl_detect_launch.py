0634d493521f0a5805a0f420a5566a3723c95889
Sebastiano Vascon <xwasco@users.noreply.github.com>
2024-10-09 16:29:43 +0200

    Update rl_detect_launch.py
    added comments on parameters

--- src/rl_detect_bringup/launch/rl_detect_launch.py
+++ src/rl_detect_bringup/launch/rl_detect_launch.py
@@ -11,12 +11,12 @@ def generate_launch_description():
             name='intel_publisher_yolo_3dbbox_node2',
             output='screen',
             parameters=[
-                {"segmentation": "mobile_sam.pt"},
-                {"detection": "yolov8l-world.pt"},
-                {"classes": "bottle, monitor, dustbin, cup, chair, backpack, laptop"},
-                {'output_markers': 'True'},
-                {'output_pointcloud': 'True'},
-                {'output_detection_3d': 'True'}
+                {"segmentation": "mobile_sam.pt"}, ## segmentation model
+                {"detection": "yolov8l-world.pt"}, ## detection model
+                {"classes": "bottle, monitor, dustbin, cup, chair, backpack, laptop"}, ## list of classes of interests
+                {'output_markers': 'True'}, ## visualization of BBOxes in RVIZ2
+                {'output_pointcloud': 'True'}, ## visualization of segmented pointcloud of objects in RVIZ2
+                {'output_detection_3d': 'True'} ## detection messages in ROS2
             ]
         )
     ])
